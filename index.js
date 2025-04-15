4d7c9eec59e79ac99c15eb79cdd97610308df289
mannuccimattia <mannuccimattia@gmail.com>
2025-04-15 14:12:10 +0200

    [FIX] Better indentation

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// console.log("Hello World")
-
-// const x = process.argv[2] ? process.argv[2] : "";
-// const y = process.argv[3] ? process.argv[3] : "";
-
-// console.log(`${x} ${y}`);
-console.log(process.argv)
 let output = [];
-for(let i =2; i< process.argv.length; i++){
-if(process.argv[i] !== undefined){
-  output.push(process.argv[i]);
+
+for (let i = 2; i < process.argv.length; i++) {
+  if (process.argv[i] !== undefined) {
+    output.push(process.argv[i]);
+  };
 };
-};
+
 console.log(output.join(" "));
